2695c781fdebf6e15393a09fb3277d3c5d903e71
Hquan912 <92044119+Hquan912@users.noreply.github.com>
2021-11-11 20:43:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
+
 const CONFIG = {
-    introTitle: 'Gửi 3 bạn đáng iu!',
-    introDesc: `   Chúng tôi là 3 th tồi tệ Ngô, Quân, Hải
-    Hẫy bấm vào nút bên dưới để nhận một món quà siu to khổng lồ `,
-    btnIntro: 'đây nè 3 má',
-    title: 'Lời xin lũi mụn màng từ 3 chàng trai đãng trí :( ',
-    desc: ' Tha lỗi choa chúng tôi nhé, quên tạm thời v thui chớ iu quý 3 bạn nhất, tha lỗi ik bao 1 trầu lẩu nhé ',
-    btnYes: 'Oke tha lỗi cho lần này nhé',
-    btnNo: 'Không, cút đi 3 th tồi tệ',
-    question:'3 bạn có muốn chúng tôi thay đổi điều gì hom',
-    btnReply: 'gửi cho chúng tui',
-    reply: 'không đâu 3 cậu là tuyệt vời nhất <33333333',
-    mess: 'Hehehe chúng tui biết mà. Không chỉ tuyệt mà còn đẹp zai nữa nhé',
-    messDesc: 'Tối mai chúng ta cùng nhau bonding tâm hự nhé',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/hoang.quan.091203/'  
+    introTitle: Xin chèoo! ,
+    introDesc: `Gởi choa bạn Quềnh aka bé mèo bíu siu đáng iu
+    Ấn vô dưới để cóa nhiều điều bất ngờ nhé `,
+    btnIntro: 'đây nè má',
+    title: ' Yay chúc mừng sinh nhật tủi mừi tắm cụa bé mèo méo meo nhaa =))) ',
+    desc: ' Chúc bạn Quỳnh tuổi mới xinh đẹp mạnh khoẻ học roải và đỗ AEISEC nhaa.
+Không biết là tui coá thể mời banh Quềnh đi chơi 1 bủi khum nhị 😳🙄😳 
+ ',
+    btnYes: ' Okii đoòng ý nhaa :> ',
+    btnNo: ' không tự đi 1 mình ik (đố ấn được zô đây keke) ',
+    question:' Bạn Quềnh coá mún tui làm gì tặng sinh nhật khum nè, hoặc là coá mún nói rì vứi tui hem hêh ',
+    btnReply: ' day ne ',
+    reply: ' cám ưn hquan đẹp trai đáng iu cute nhiều nha ',
+    mess: ' Tôi biết mà hehe! Anw chúc mừng sinh nhật nghenn mong là bọn mình sẽ nói chuyện nhiều hơn ha ',
+    messDesc: ' Happy birthdayyy tủi 18 siêu đẹp nha!!! ',
+    btnAccept: 'Okiiiii lun ',
+    messLink: 'https://www.facebook.com/messages/t/4006798229445982'
 }
